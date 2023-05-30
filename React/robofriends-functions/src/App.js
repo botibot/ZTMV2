5628245f360efbb4234ba1b03e98c90c40e1305e
cayorch <cayorch@gmail.com>
2023-05-29 22:43:40 -0600

    Learning

---
+++ React/robofriends-functions/src/App.js
@@ -0,0 +1,62 @@
+import { Component } from "react";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="app-title"> Monsters </h1>
+    </div>
+  );
+};
+
+/* class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+  onSearchChange = (event) => {
+    console.log(event.target.value);
+    const searchField = event.target.value.toLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
+  render() {
+    const { monsters, searchField } = this.state;
+    const { onSearchChange } = this;
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLowerCase().includes(searchField);
+    });
+
+    return (
+      <div className="App">
+        <h1 className="app-title"> Monsters </h1>
+        <SearchBox
+          className="monsters-search-box"
+          onChangeHandler={onSearchChange}
+          placeholder="Search Monsters"
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+} */
+
+export default App;
